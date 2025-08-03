3f719199d9ddb26391c7926430448f5ac828295d
Zemel <shuiwy_333@163.com>
2025-08-03 17:33:20 +0800

    init project

---
+++ meiduo_mall/libs/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2025/8/3 17:31
+# @Author  : 老冰棍
+# @File    : __init__.py.py
+# @Software: PyCharm
--- meiduo_mall/meiduo_mall/settings.py
+++ meiduo_mall/meiduo_mall/settings.py
@@ -69,6 +69,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'meiduo_mall.wsgi.application'
 
+##
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
