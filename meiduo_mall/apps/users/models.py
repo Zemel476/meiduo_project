21be88f4885482e7b41aeb0b2b602a8b46baac7d
Zemel <shuiwy_333@163.com>
2025-08-03 22:24:58 +0800

    替换系统User模型

--- meiduo_mall/apps/users/models.py
+++ meiduo_mall/apps/users/models.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
+# django 自带用户模型，有密码加密验证功能
+
+class User(AbstractUser):
+    mobile = models.CharField(max_length=11, unique=True) # unique 唯一
+
+    class Meta:
+        db_table = 'tb_users'
+        verbose_name = '用户管理'
+        verbose_name_plural = verbose_name
--- meiduo_mall/meiduo_mall/settings.py
+++ meiduo_mall/meiduo_mall/settings.py
@@ -190,3 +190,6 @@ LOGGING = {
         }
     }
 }
+
+
+AUTH_USER_MODEL = 'users.User'
