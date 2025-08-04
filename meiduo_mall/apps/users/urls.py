2902ef49a64d956fe83a391b84ddc0f723a46928
Zemel <shuiwy_333@163.com>
2025-08-04 10:06:18 +0800

    判断用户名是否重复功能实现

--- meiduo_mall/apps/users/urls.py
+++ meiduo_mall/apps/users/urls.py
@@ -3,3 +3,11 @@
 # @Author  : 老冰棍
 # @File    : urls.py
 # @Software: PyCharm
+
+from django.urls import path
+
+from apps.users.views import UsernameCountView
+
+urlpatterns = [
+    path('username/<username>', UsernameCountView.as_view(), name='username'),
+]
--- meiduo_mall/apps/users/views.py
+++ meiduo_mall/apps/users/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.views import View
+
+from apps.users.models import User
+
 
 # Create your views here.
+
+class UsernameCountView(View):
+
+    def get(self, request, username):
+        count = User.objects.filter(username=username).count()
+
+        return JsonResponse({'count':count, 'code':200, 'errmsg': ''})
